3b87862ee96f2cf0702e0501311be68c37eb9ad4
Punksnation <cyberpunk26@gmail.com>
2023-07-25 11:31:40 -0700

    1. Education is when you read the fine print. Experience is what you get if you don't

---
+++ _puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _puts - print a buffer
+ * @buffer: buffer passed
+ * @size: number of bytes to print
+ */
+
+void _puts(char *buffer, int size)
+{
+        write(1, buffer, size);
+}
--- h__functions.c
+++ h__functions.c
@@ -48,14 +48,3 @@ char *_empty(char c)
 	str[2] = '\0';
 	return (p);
 }
-
-/**
- * _puts - print a buffer
- * @buffer: buffer passed
- * @size: number of bytes to print
- */
-
-void _puts(char *buffer, int size)
-{
-	write(1, buffer, size);
-}
